c305aa929365574fde0db6c70f0b78bc76c7d59c
Daniela Mendez <dannyemet@gmail.com>
2022-09-26 10:56:03 -0500

    feat: uso de script externo

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-<script>
-console.log("Saoko papi saoko");
-document.getElementById("generation").style.color = "pink";
-function colorTitulo() {
-    console.log("Pulso el boton de Generation");
+    console.log("Saoko papi saoko");
+    document.getElementById("generation").style.color = "pink";
+    function colorTitulo() {
+        console.log("Pulso el boton de Generation");
     document.getElementById("generation").style.color = "blue";
 }
 
-</script>
